9c0645c506d2a0c934f95cfb8da19c51d60b0e50
Yoga Hendrapratama <yhepra@gmail.com>
2023-01-01 13:02:51 +0700

    Add Data To Student (Single)

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using EksperimentWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EksperimentWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var student = new Student()
+            {
+                Id = 1,
+                Name = "Romi Kusuma Bakti",
+                Address = "Boyolali",
+                PhoneNumber = "08001"
+            };
+
+            return View(student);
+        }
+    }
+}
