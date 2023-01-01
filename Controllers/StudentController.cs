9cfd52bec93af28e898341ea9fb65a914aee097a
Yoga Hendrapratama <yhepra@gmail.com>
2023-01-01 13:30:06 +0700

    Add Data to Model (Array)

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -7,22 +7,15 @@ namespace EksperimentWebApplication.Controllers
     {
         public IActionResult Index()
         {
-            var student = new List<Student>();
-            student.Add(new Student
+            var student = new Student[]
             {
-                Id = 1,
-                Name = "Romi Kusuma Bakti",
-                Address = "Boyolali",
-                PhoneNumber = "08001"
-            });
-
-            student.Add(new Student
-            {
-                Id = 2,
-                Name = "Agus Rahman Hidayat",
-                Address = "Ciamis",
-                PhoneNumber = "08002"
-            });
+                new Student{ Id = 1, Name = "Romi Kusuma Bakti", Address = "Boyolali", PhoneNumber = "08001"},
+                new Student{ Id = 2, Name = "Agus Rahman Hidayat", Address = "Ciamis", PhoneNumber = "08002"},
+                new Student{ Id = 3, Name = "Dimas Firmansyah", Address = "Banjar", PhoneNumber = "08003"},
+                new Student{ Id = 4, Name = "Dede Adam Alamsyah", Address = "Banjarsari", PhoneNumber = "08004"},
+                new Student{ Id = 5, Name = "Sawaludin", Address = "Padangsidimpuan", PhoneNumber = "08005"}
+            };
+            
 
             return View(student);
         }
