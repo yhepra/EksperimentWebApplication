eaf6041d468c619955476b368561f840737f6a5e
Yoga Hendrapratama <yhepra@gmail.com>
2023-01-01 13:24:46 +0700

    Add Data to Model (List)

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -7,13 +7,22 @@ namespace EksperimentWebApplication.Controllers
     {
         public IActionResult Index()
         {
-            var student = new Student()
+            var student = new List<Student>();
+            student.Add(new Student
             {
                 Id = 1,
                 Name = "Romi Kusuma Bakti",
                 Address = "Boyolali",
                 PhoneNumber = "08001"
-            };
+            });
+
+            student.Add(new Student
+            {
+                Id = 2,
+                Name = "Agus Rahman Hidayat",
+                Address = "Ciamis",
+                PhoneNumber = "08002"
+            });
 
             return View(student);
         }
